d5bcd7772aed6ec2130ed6a0cf84d1eb69701ce4
Rayzr522 <rayzr522@gmail.com>
2020-03-09 22:54:31 -0400

    Add missing ShopItem#getItem() method

--- src/main/java/net/brcdev/shopgui/shop/ShopItem.java
+++ src/main/java/net/brcdev/shopgui/shop/ShopItem.java
@@ -2,9 +2,12 @@ package net.brcdev.shopgui.shop;
 
 import net.brcdev.shopgui.player.PlayerData;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
 
 public interface ShopItem {
   double getBuyPriceForAmount(Shop shop, Player player, PlayerData playerData, int amount);
 
   double getSellPriceForAmount(Shop shop, Player player, PlayerData playerData, int amount);
+
+  ItemStack getItem();
 }
